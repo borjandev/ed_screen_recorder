19cf85a6a851020a127e13e2a14659ed1af09937
Borjan Trajanoski <borjandev@gmail.com>
2023-02-02 03:15:43 +0100

    [ios] ~ enforce portrait recording

--- ios/Classes/SwiftEdScreenRecorderPlugin.swift
+++ ios/Classes/SwiftEdScreenRecorderPlugin.swift
@@ -55,19 +55,7 @@ public class SwiftEdScreenRecorderPlugin: NSObject, FlutterPlugin {
         self.eventName=String("startRecordScreen")
         var width = args?["width"]; // in pixels
         var height = args?["height"] // in pixels
-        if UIDevice.current.orientation.isLandscape {
-            if(width == nil || width is NSNull) {
-                width = Int32(UIScreen.main.nativeBounds.height); // pixels
-            } else {
-                width = Int32(height as! Int32);
-            }
-            if(height == nil || height is NSNull) {
-                height = Int32(UIScreen.main.nativeBounds.width); // pixels
-            } else {
-                height = Int32(width as! Int32);
-            }
-        }else{
-            if(width == nil || width is NSNull) {
+         if(width == nil || width is NSNull) {
                 width = Int32(UIScreen.main.nativeBounds.width); // pixels
             } else {
                 width = Int32(width as! Int32);
@@ -77,7 +65,6 @@ public class SwiftEdScreenRecorderPlugin: NSObject, FlutterPlugin {
             } else {
                 height = Int32(height as! Int32);
             }
-        }
         self.success=Bool(startRecording(width: width as! Int32 ,height: height as! Int32,dirPathToSave:(self.dirPathToSave as NSString) as String));
         self.startDate=Int(NSDate().timeIntervalSince1970 * 1_000)
         myResult = result
